232a70f7120d2a4242efdbe791d787c522be5c53
Csizmarik Norbert <Csizmarik.Norbert@biztositas.hu>
2017-09-13 12:54:48 +0200

    arch(store): @ngrx/store integration to the application
    The Redux inspired @ngrx/store state management framework interation
    
    Signed-off-by: Csizmarik Norbert <Csizmarik.Norbert@biztositas.hu>

--- src/app/admin.module.ts
+++ src/app/admin.module.ts
@@ -1,10 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {StoreModule} from '@ngrx/store';
 import {AppComponent} from './admin.component';
 import {AdminRoutingModule} from './admin-routing.module';
 import {DashboardModule} from './components/dashboard/dashboard.module';
 import {RouterModule} from '@angular/router';
+import {adminReducer} from './store/admin.reducer';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import {RouterModule} from '@angular/router';
       BrowserModule,
       RouterModule,
       DashboardModule,
-      AdminRoutingModule
+      AdminRoutingModule,
+      StoreModule.forRoot(adminReducer)
   ],
   providers: [],
   bootstrap: [AppComponent]
