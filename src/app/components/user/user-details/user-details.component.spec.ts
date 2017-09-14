c7c26da41ffd4cca1f5131191064e61bae80b9a0
Csizmarik Norbert <Csizmarik.Norbert@biztositas.hu>
2017-09-14 08:33:14 +0200

    test(user/details): Required services for UserDetails component in the test environment
    Signed-off-by: Csizmarik Norbert <Csizmarik.Norbert@biztositas.hu>

--- src/app/components/user/user-details/user-details.component.spec.ts
+++ src/app/components/user/user-details/user-details.component.spec.ts
@@ -1,6 +1,11 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UserDetailsComponent} from './user-details.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {UserService} from '../../../services/user/user.service';
+import {HttpModule} from '@angular/http';
+import {StoreModule} from '@ngrx/store';
+import {adminReducer} from '../../../store/admin.reducer';
 
 describe('UserDetailsComponent', () => {
     let component: UserDetailsComponent;
@@ -8,6 +13,14 @@ describe('UserDetailsComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            imports: [
+                ReactiveFormsModule,
+                HttpModule,
+                StoreModule.forRoot(adminReducer),
+            ],
+            providers: [
+                UserService
+            ],
             declarations: [UserDetailsComponent]
         })
             .compileComponents();
